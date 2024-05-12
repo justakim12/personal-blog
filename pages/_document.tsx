87aeca78ef45fb31628b3c00febc3d06bbf5ccf6
kyungil.david <kyungil.david@navercorp.com>
2024-05-12 15:22:56 +0900

    Add nav linkgs and profile image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
+            <link rel='shortcut icon' href='/profile_image.png' />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='favicon.png'
+              href='profile_image.png'
             />
 
             <link rel='manifest' href='/manifest.json' />
--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '34a443fc036b4595ac4aab0799277b45'
+    },
+    {
+      title: 'Contact',
+      pageId: '5da5aa5ad3fb4cd0be9917117042587b'
+    }
+  ]
 })
